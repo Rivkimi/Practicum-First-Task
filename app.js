8cd8d7b261ba5b2dd9954fab6e9db61eaf26298b
רבקי מינצברג <Rivkimi>
2024-03-05 11:53:45 +0200

    secondBranch

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express = require('express')
-// const mongoose = require('mongoose')
-const User=require('./modols/User')
-// const u=new User()
-// const us={
-//     name:"rivka",
-//     email:"05552@gmail.com",
-//     phone:"4540500"
-// }
-// u.create(us)
+const User=require('./models/User')
 const userRoutes = require('./routers/User')
 const app= express()
 app.use(express.json())
