2cb0639d17cf4a0a3aa944c51e9b2692ea0b65f1
רבקי מינצברג <Rivkimi>
2024-03-05 10:50:25 +0200

    secondBranch

--- routers/User.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getAllUsers,createUser,getUserById,deleteUserById} =require('../controllers/User')
-router.get('/',getAllUsers)
-router.post('/',createUser)
-router.get('/:id',getUserById)
-router.delete('/:id',deleteUserById)
-module.exports = router;
